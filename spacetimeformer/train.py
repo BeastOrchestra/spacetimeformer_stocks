0a7b108e4ad58472763184df65bff79ccba7d611
BeastOrchestra <50928335+BeastOrchestra@users.noreply.github.com>
2024-01-19 15:40:06 -0600

    No Changes

--- spacetimeformer/train.py
+++ spacetimeformer/train.py
@@ -801,15 +801,15 @@ def main(args):
     # Data Preparation
     if args.dset == "stocks":
         # Custom DataLoader for 'stocks'
-        args.null_value = null_val
-        args.pad_value = pad_val
+        args.null_value = NULL_VAL
+        # args.pad_value = pad_val
         train_loader = DataLoader(TimeSeriesDataset(data_folder='spacetimeformer/data/train', context_length=args.context_points, forecast_length=args.target_points), batch_size=args.batch_size, shuffle=True)
         test_loader = DataLoader(TimeSeriesDataset(data_folder='spacetimeformer/data/test', context_length=args.context_points, forecast_length=args.target_points), batch_size=args.batch_size, shuffle=False)
         oos_loader = DataLoader(TimeSeriesDataset(data_folder='spacetimeformer/data/oos', context_length=args.context_points, forecast_length=args.target_points), batch_size=args.batch_size, shuffle=False)
     else:
         # Standard DataModule for other datasets
         data_module, inv_scaler, scaler, null_val, plot_var_idxs, plot_var_names, pad_val = create_dset(args)
-        args.null_value = null_val
+        args.null_value = None
         args.pad_value = pad_val
 
     # Model Training and Evaluation
