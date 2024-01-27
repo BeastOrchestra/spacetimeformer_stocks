ff9ffd56223aa6ac02c57c07785121067799dfdf
BeastOrchestra <50928335+BeastOrchestra@users.noreply.github.com>
2024-01-26 20:44:48 -0600

    shifting dset index by 1 in 'within_file_idx' variable +1

--- spacetimeformer/TimeSeriesDataset.py
+++ spacetimeformer/TimeSeriesDataset.py
@@ -24,7 +24,8 @@ class TimeSeriesDataset(Dataset):
 
     def __getitem__(self, idx):
         file_index = np.where(self.cumulative_lengths > idx)[0][0] - 1
-        within_file_idx = idx - self.cumulative_lengths[file_index]
+        # within_file_idx = idx - self.cumulative_lengths[file_index] # original
+        within_file_idx = idx - self.cumulative_lengths[file_index] + 1
         context = self.data_files[file_index][within_file_idx:within_file_idx+self.context_length]
         forecast = self.data_files[file_index][within_file_idx+self.context_length:within_file_idx+self.context_length+self.forecast_length]
         return torch.tensor(context, dtype=torch.float), torch.tensor(forecast, dtype=torch.float)
