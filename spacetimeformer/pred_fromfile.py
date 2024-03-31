84093f29f0341e2bb073e56afa92ebc28263b11e
BeastOrchestra <50928335+BeastOrchestra@users.noreply.github.com>
2024-03-31 17:28:47 -0500

    changed model reference

--- spacetimeformer/pred_fromfile.py
+++ spacetimeformer/pred_fromfile.py
@@ -402,9 +402,9 @@ def main(args):
 # 2/14
     # output_path = "/Users/alecjeffery/Documents/Playgrounds/Python/largeModels/60c73v100ctx10tgt5e6d512_2048.pth"
 # 2/15    
-    # output_path = "/Users/alecjeffery/Documents/Playgrounds/Python/largeModels/60c83v.pth"
+    output_path = "/Users/alecjeffery/Documents/Playgrounds/Python/largeModels/mar29_2024_p57_v71.pth"
 # 2/24
-    output_path = "/Users/alecjeffery/Documents/Playgrounds/Python/largeModels/feb24_2024.pth"
+    # output_path = "/Users/alecjeffery/Documents/Playgrounds/Python/largeModels/feb24_2024.pth"
 
 
     # gdown.download(url, output_path, quiet=False)
