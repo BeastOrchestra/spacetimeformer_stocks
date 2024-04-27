643da57f483b56c9bc8f8f4ba202d14c6ace496b
BeastOrchestra <50928335+BeastOrchestra@users.noreply.github.com>
2024-04-27 15:21:31 -0500

    Data udates and new model trained on lambda

--- spacetimeformer/pred_fromfile.py
+++ spacetimeformer/pred_fromfile.py
@@ -400,13 +400,10 @@ def main(args):
     forecaster = create_model(args)
     forecaster = forecaster.to(device)  # Move the model to the specified device
 
-# 2/14
-    # output_path = "/Users/alecjeffery/Documents/Playgrounds/Python/largeModels/60c73v100ctx10tgt5e6d512_2048.pth"
-# 2/15    
+# Current best 
     output_path = "/Users/alecjeffery/Documents/Playgrounds/Python/largeModels/mar29_2024_p57_v71.pth"
-# 2/24
-    # output_path = "/Users/alecjeffery/Documents/Playgrounds/Python/largeModels/feb24_2024.pth"
-
+# 4/22
+    output_path = "/Users/alecjeffery/Documents/Playgrounds/Python/largeModels/V75P60_4_25.pth"
 
     # gdown.download(url, output_path, quiet=False)
 
